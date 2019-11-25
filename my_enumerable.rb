1486ef84e7554f9fe4141b8895e46959e33ee625
Amadou Ibrahim <amadou140589@gmail.com>
2019-11-25 21:28:41 +0100

    my_count method added

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,65 +1,75 @@
+# frozen_string_literal: true
+
 module Enumerable
-    def my_each
-        if block_given?
-            for i in 0...self.length do
-                yield(self[i])
-            end
-        end
-        self
+  def my_each
+    if block_given?
+      (0...length).each do |i|
+        yield(self[i])
+      end
     end
+    self
+  end
 
-    def my_each_with_index
-        if block_given?
-            for i in 0...self.length do
-                yield(self[i],i)
-                
-            end
-        end
-        self
+  def my_each_with_index
+    if block_given?
+      (0...length).each do |i|
+        yield(self[i], i)
+      end
     end
+    self
+  end
 
-    def my_select
-        if block_given?
-            r=[]
-            self.my_each do |x|
-                if yield(x)
-                    r << x
-                end
-            end
-            r
-        end
+  def my_select
+    if block_given?
+      r = []
+      my_each do |x|
+        r << x if yield(x)
+      end
+      r
     end
+  end
 
-    def my_all?
-        if block_given?
-            self.my_each do |x|
-                return false if !yield(x)
-            end
-            return true
-        end
-        
+  def my_all?
+    if block_given?
+      my_each do |x|
+        return false unless yield(x)
+      end
+      true
     end
-    
-    def my_any?
-        if block_given?
-            my_each do |x|
-                return true if yield(x)
-            end
-            false
-        end
-    end
+  end
 
-    def my_none?
-        if block_given?
-            my_each do |x|
-                return false if yield(x)
-            end
-            true
-        end
+  def my_any?
+    if block_given?
+      my_each do |x|
+        return true if yield(x)
+      end
+      false
     end
-    
+  end
+
+  def my_none?
+    if block_given?
+      my_each do |x|
+        return false if yield(x)
+      end
+      true
+    end
+  end
+
+  def my_count(number = nil)
+    count = 0
+    if !block_given? && number.nil?
+      count = length
+    elsif !block_given? && !number.nil?
+      my_each do |x|
+        count += 1 if x == number
+      end
+    else
+
+      my_each do |x|
+        count += 1 if yield(x)
+      end
+    end
+    count
+  end
 end
-
-p [2,3,8,5,15].my_all?{ |x| x<10 }
-
-p [2,3,8,5,15].all?{ |x| x<10}

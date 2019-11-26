0eef46432bb0edbf117f3543ef332c3e48b3b7af
Amadou Ibrahim <amadou140589@gmail.com>
2019-11-26 03:51:05 +0100

    my_inject method added with tests

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -2,58 +2,62 @@
 
 module Enumerable
   def my_each
-    if block_given?
-      (0...length).each do |i|
-        yield(self[i])
-      end
+    return to_enum unless block_given?
+    for i in 0...self.length do
+        yield self[i] 
     end
     self
   end
+  
 
   def my_each_with_index
-    if block_given?
-      (0...length).each do |i|
-        yield(self[i], i)
-      end
-    end
+    return to_enum unless block_given?
+
+    (0...length).each { |i| yield self[i], i }
     self
   end
 
   def my_select
-    if block_given?
-      r = []
-      my_each do |x|
-        r << x if yield(x)
-      end
-      r
-    end
+    return to_enum unless block_given?
+    r = []
+    self.my_each { |x| r << x if yield x }
+    r
   end
 
-  def my_all?
+  def my_all?(pattern = nil)
+    r = true
     if block_given?
-      my_each do |x|
-        return false unless yield(x)
-      end
-      true
+      my_each { |x| r = false unless yield x }
+    elsif pattern
+      my_each { |x| r = false unless pattern === x }
+    else
+      my_each { |x| r = false unless x }
     end
+    r
   end
 
-  def my_any?
+  def my_any?(pattern=nil)
+    r=false
     if block_given?
-      my_each do |x|
-        return true if yield(x)
-      end
-      false
+        my_each {|x| r=true if yield x}
+    elsif pattern
+        my_each { |x| r = true if pattern === x }
+    else
+    my_each { |x| r = true if  x}
     end
+    r
   end
 
-  def my_none?
+  def my_none?(pattern=nil)
+    r = true
     if block_given?
-      my_each do |x|
-        return false if yield(x)
-      end
-      true
-    end
+        my_each { |x| r = false if yield x}
+    elsif pattern
+        my_each { |x| r = false if x === pattern}
+    else
+        my_each { |x| r = false if  x }
+    end    
+    r
   end
 
   def my_count(number = nil)
@@ -74,12 +78,30 @@ module Enumerable
   end
 
   def my_map
-    r=[]
-    self.my_each do |x|
-        r << yield(x)
+    return to_enum unless block_given?
+    r = []
+    my_each do |x|
+      r << yield(x)
     end
     r
   end
-  
+
+  def my_inject(*params)
+    # if (initial, sym)
+    if init && symb && symb.is_a?(Symbol)
+      my_each do |x|
+        init = init.method(symb).call(x)
+      end
+    # if (symb)
+    elsif init && init.is_a?(Symbol) && symb.nil?
+      memo, *elements = self
+      elements.my_each do |x|
+        memo = memo.method(init).call(x)
+      end
+      memo
+    end
+    init
+  end
 end
 
+#p %w{ant bear cat}.my_none?(/d/)                        #=> true
